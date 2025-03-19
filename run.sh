b98df24779857a306240da29cb4dc70e1c32144c
Karan Vahi <vahi@isi.edu>
2025-03-19 12:49:01 -0400

    update to suchi latest config

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-./chess-qmb.py --execution-site sge --raw-base-dir /nfs/chess/id4b/2024-1/ramshaw-3435-b --calibration-base-dir /nfs/chess/id4baux/2024-1/\
-ramshaw-3435-b
+#./chess-qmb.py --execution-site sge --raw-base-dir /nfs/chess/id4b/2024-1/ramshaw-3435-b --calibration-base-dir /nfs/chess/id4baux/2024-1/ramshaw-3435-b
+
+./chess-qmb.py --execution-site sge --raw-base-dir /nfs/chess/id4b/2024-3/hufnagel-3957-c --calibration-base-dir /nfs/chess/id4baux/2024-3/hufnagel-3957-c 
